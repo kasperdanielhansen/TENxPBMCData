cb78d06650051a4490502f5ccd64ab5c82489685
Stephanie Hicks <stephaniechicks@gmail.com>
2018-08-23 10:15:17 -0400

    added code to pull data from ExperimentHub

--- R/TENxPBMCData.R
+++ R/TENxPBMCData.R
@@ -1,5 +1,26 @@
-TENxPBMCData <- function(dataset = c("68k"))
+TENxPBMCData <- function(sample_name = c("pbmc4k"))
+  ## Download HDF5 (dense assay) and RDS (row and column
+  ## annotations) files from ExperimentHub, compose into a
+  ## SingleCellExperiment object.
 {
-    dataset <- match.arg(dataset)
-    ## Reurn a SingleCellExperiment
+    hub <- ExperimentHub()
+    base <- sample_name
+    
+    ## row and column data
+    rdatapath <- paste0("TENxPBMCData/", base, "_rowData.rds")
+    rowData <- query(hub, rdatapath)[[1]]
+    
+    suppressMessages({
+      rdatapath <- paste0("TENxPBMCData/", base, "_colData.rds")
+      colData <- query(hub, rdatapath)[[1]]
+      
+      ## HDF5, from ExperimentHub:
+      rdatapath <- paste0("TENxPBMCData/", base, "_rectangular.h5")
+      h5file <- query(hub, rdatapath)[[1]]
+    })
+    h5array <- HDF5Array(h5file, "counts")
+    
+    SingleCellExperiment(
+      list(counts = h5array), rowData = rowData, colData = colData
+    )
 }
